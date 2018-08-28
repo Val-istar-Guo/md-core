841f791973b0d8e2d9b9c7a387edc7ebe1e10834
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-08-29 00:09:15 +0800

    md-core/utils integrated combineString function

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,5 +1,6 @@
 export { default as combine } from './combine'
 export { default as middleware } from './middleware'
 export { default as preprocessor } from './preprocessor'
-export { default as htmlEncode } from './htmlEncode';
-export { default as htmlDecode } from './htmlDecode';
+export { default as htmlEncode } from './htmlEncode'
+export { default as htmlDecode } from './htmlDecode'
+export { combineString } from '../utils'
