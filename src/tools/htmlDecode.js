63668d52a7fe57b8fc2fe9d7ed085c55b8ed7fc8
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-08-31 00:11:04 +0800

    fix the problem that htmlCode function only encodes the first escape character

--- src/tools/htmlDecode.js
+++ src/tools/htmlDecode.js
@@ -1,11 +1,11 @@
 export default str => {
   if (str.length ===0) return
 
-  str = str.replace('&amp;', '&')
-  str = str.replace('&lt;', '<')
-  str = str.replace('&gt;', '>')
-  str = str.replace('&#39;', '\'')
-  str = str.replace('&quot;', '\"')
+  str = str.replace(/&amp;/g, '&')
+  str = str.replace(/&lt;/g, '<')
+  str = str.replace(/&gt;/g, '>')
+  str = str.replace(/&#39;/g, '\'')
+  str = str.replace(/&quot;/g, '\"')
 
   return str
 }
